c0c778290501e8226a651b9e0c91532a53fbdab9
mateng <mateng@beiwaionline.com>
2022-10-05 12:00:56 +0800

    更新

---
+++ Hikids/Controllers/BaseVC/MenuVC/Controllers/MenuWalkVC/HWalkMenuVC.h
@@ -0,0 +1,16 @@
+//
+//  HWalkMenuVC.h
+//  Hikids
+//
+//  Created by 马腾 on 2022/10/5.
+//
+
+#import "HBaseMenuVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HWalkMenuVC : HBaseMenuVC
+
+@end
+
+NS_ASSUME_NONNULL_END
