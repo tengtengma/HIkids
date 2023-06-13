9b3874feeeafc86d9134ddfe84765ab630efc116
马腾 <mateng@matengdeMBP-2.lan>
2023-06-14 00:38:53 +0800

    更新 坐标切换状态

---
+++ Hikids/Controllers/ReportVC/Views/HWalkDangerView.h
@@ -0,0 +1,16 @@
+//
+//  HWalkDangerView.h
+//  Hikids
+//
+//  Created by 马腾 on 2023/6/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HWalkDangerView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hikids/Controllers/ReportVC/Views/HWalkDangerView.m
@@ -0,0 +1,20 @@
+//
+//  HWalkDangerView.m
+//  Hikids
+//
+//  Created by 马腾 on 2023/6/13.
+//
+
+#import "HWalkDangerView.h"
+
+@implementation HWalkDangerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
